01336fa5636b7ca71919a23350050d78d7a8234e
ailobe <aitorlopezbeltran@gmail.com>
2019-02-14 16:55:34 +0100

    Add migration after upgrading decidim 0.16.0

---
+++ db/migrate/20190214155425_remove_decidim_surveys_tables_after_migrate_to_decidim_forms.rb
@@ -0,0 +1,15 @@
+class RemoveDecidimSurveysTablesAfterMigrateToDecidimForms < ActiveRecord::Migration[5.2]
+  def up
+    # Drop tables
+    drop_table :decidim_surveys_survey_answers
+    drop_table :decidim_surveys_survey_answer_choices
+    drop_table :decidim_surveys_survey_answer_options
+    drop_table :decidim_surveys_survey_questions
+
+    # Drop columns from surveys table
+    remove_column :decidim_surveys_surveys, :title
+    remove_column :decidim_surveys_surveys, :description
+    remove_column :decidim_surveys_surveys, :tos
+    remove_column :decidim_surveys_surveys, :published_at
+  end
+end
