c4b5d91af1abc871571fcc00922308188113d39a
ailobe <aitorlopezbeltran@gmail.com>
2019-02-14 09:57:26 +0100

    Add decidim:delete_data_portability_files to schedule
    Thanks to review

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# frozen_string_literal: true
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+env :PATH, ENV['PATH']
 
-# Learn more: http://github.com/javan/whenever
+every :sunday, at: '4:00 am' do
+  rake 'decidim:delete_data_portability_files'
+end
 
-every 1.day, at: '2:00 am' do # 1.minute 1.day 1.week 1.month 1.year is also supported
-  rake "decidim:metrics:all"
+every 1.day, at: '2:00 am' do
+  rake 'decidim:metrics:all'
 end
