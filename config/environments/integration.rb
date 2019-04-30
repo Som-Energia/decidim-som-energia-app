061854a35e8359940fd7660e0847d444d2811bd3
Isaac Massot <isaac.mg@coditramuntana.com>
2019-04-30 10:57:42 +0200

    working on mailer

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(:harmony => true)
-
+  config.active_job.queue_adapter = :delayed_job
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.active_job.queue_adapter = :delayed_job
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
