cdb99052a587ab8ddd0378065febb9d4fc19e927
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-03-21 21:13:14 +0100

    Add Question results summary page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   mount Decidim::Core::Engine => "/"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/questions/:slug/summary", to: "decidim/consultations/question_summaries#show", as: "question_summary"
+  get "/question_summaries/preview", to: "decidim/consultations/question_summaries#preview" if Rails.env.development?
 end
