7c2998cf701d7511a2d855fbacbbdebc60007388
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-03-27 10:43:44 +0100

    Add Consultation summary page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/consultations/:slug/summary", to: "decidim/consultations/consultation_summaries#show", as: "consultation_summary"
   get "/questions/:slug/summary", to: "decidim/consultations/question_summaries#show", as: "question_summary"
-  get "/question_summaries/preview", to: "decidim/consultations/question_summaries#preview" if Rails.env.development?
+
+  if Rails.env.development?
+    get "/consultation_summaries/preview", to: "decidim/consultations/consultation_summaries#preview"
+    get "/question_summaries/preview", to: "decidim/consultations/question_summaries#preview"
+  end
 end
