e0063a12cbf02c853bb21a47c7fb715321f68e37
Ivan Vergés <ivan@platoniq.net>
2021-04-06 15:27:55 +0200

    add homepage spec

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -6,6 +6,7 @@ describe "Visit the home page", type: :system do
   let(:official_url) { "http://mytesturl.lvh.me" }
   let(:organization) { create(:organization, official_url: official_url) }
   let!(:local_groups) { create :assemblies_type, id: 5 }
+  let(:slug) { "SomAG-xx" }
 
   before do
     switch_to_host(organization.host)
@@ -49,6 +50,34 @@ describe "Visit the home page", type: :system do
     end
   end
 
+  context "when there is normal participatory processes" do
+    let!(:participatory_process) { create(:participatory_process, :published, organization: organization) }
+
+    it "renders the expected menu" do
+      visit decidim.root_path
+
+      within ".main-nav" do
+        expect(page).to have_content("Home")
+        expect(page).to have_content("Processes")
+        expect(page).not_to have_content("General Assemblies")
+      end
+    end
+  end
+
+  context "when there is alternative assemblies" do
+    let!(:participatory_process) { create(:participatory_process, :published, organization: organization) }
+    let!(:participatory_process2) { create(:participatory_process, :published, slug: slug, organization: organization) }
+
+    it "renders the expected menu" do
+      visit decidim.root_path
+
+      within ".main-nav" do
+        expect(page).to have_content("Home")
+        expect(page).to have_content("Processes")
+        expect(page).to have_content("General Assemblies")
+      end
+    end
+  end
   # it "has matomo tracker" do
   #   expect(page.execute_script("return typeof window._paq")).not_to eq("undefined")
   #   expect(page.execute_script("return typeof window._paq")).to eq("object")
