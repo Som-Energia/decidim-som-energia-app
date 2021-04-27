8aab6bb67e43337c528f94947179959f91446103
Aitor Lopez Beltran <aitorlopezbeltran@gmail.com>
2021-04-27 09:26:04 +0200

    Allow participatory space filters to use alternative paths (#74)
    Co-authored-by: Ivan Vergés <ivan@platoniq.net>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@
 
 module ApplicationHelper
   include Decidim::CheckBoxesTreeHelper
+
+  def participatory_space_filters_form_url
+    if request.path.remove("/").in?(
+      [
+        *ParticipatoryProcessesScoper.scoped_participatory_process_slug_prefixes.map { |hash| hash[:key] },
+        *AssembliesScoper.alternative_assembly_types.map { |hash| hash[:key] }
+      ]
+    )
+      request.path
+    else
+      url_for
+    end
+  end
 end
