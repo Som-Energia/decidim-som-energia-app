f44c983a986310502bbc4695d9fc8050e4bfeeba
Agusti B.R <hola@agusti.cat>
2022-06-14 10:48:28 +0200

    feat(sentry): add sentry config

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+Sentry.init do |config|
+  config.dsn = 'https://1dac66ab76244970812d185efdb9aeeb@o279313.ingest.sentry.io/5699904'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.traces_sample_rate = 0.5
+end
