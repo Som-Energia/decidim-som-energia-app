d6fd69637520ed3280d65dba25c6b9a61c598229
Anna Topalidi <t.opalidisanna@gmail.com>
2023-04-11 13:06:19 +0200

    change paths in the packs folder

--- app/packs/entrypoints/decidim_som_energia_app.js
+++ app/packs/entrypoints/decidim_som_energia_app.js
@@ -2,13 +2,5 @@ import "../src/application.js"
 import "../src/cable.js"
 import "../src/consultations_question_requirements_validator.js"
 
-import "../stylesheets/decidim/application.scss";
-import "../stylesheets/decidim/consultations/_consultations_question_requirements_validator.scss";
-import "../stylesheets/decidim/consultations/_question.scss";
-import "../stylesheets/decidim/consultations/_question_multiple_votes.scss.scss";
-import "../stylesheets/decidim/decidim.scss";
-import "../stylesheets/decidim/result_tables.scss";
-
-
 // Images
 require.context("../images", true)
