b70169d43bb501476690e6deb0bf91c497e3e3e6
Ivan Vergés <ivan@pokecode.net>
2023-04-24 13:15:23 +0200

    deface consultation admin results

---
+++ config/assets.rb
@@ -0,0 +1,4 @@
+
+# If you want to do the same but include the SCSS file for the admin panel's
+# main SCSS file, you can use the following method.
+Decidim::Webpacker.register_stylesheet_import("stylesheets/admin/results_tables", group: :admin)
