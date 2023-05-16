d5b232870040a4625812e3864397c19ea906ab0f
Ivan Vergés <ivan@pokecode.net>
2023-05-16 14:16:32 +0200

    fix href replacements

--- app/packs/src/cas_href_replacement.js
+++ app/packs/src/cas_href_replacement.js
@@ -1,16 +1,15 @@
 const bindCasClientRoutes = () => {
   let locale = document.documentElement.getAttribute("lang") || "es";
 
-  document.addEventListener("click", (evt) => {
-    if (evt.target.matches('a[href*="/users/sign_in"], a[href*="/users/sign_up"]')) {
-      evt.preventDefault();
-      window.location.href = `/users/cas/sign_in?locale=${locale}`;
-    }
+  document.querySelectorAll('a[href*="/users/sign_in"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_in?locale=${locale}`);
   });
-  let signOut = document.querySelector('a[href*="/users/sign_out"]')
-  if (signOut) {
-    signOut.setAttribute("href", `/users/cas/sign_out?locale=${locale}`);
-  }
+  document.querySelectorAll('a[href*="/users/sign_up"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_in?locale=${locale}`);
+  });
+  document.querySelectorAll('a[href*="/users/sign_out"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_out?locale=${locale}`);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", bindCasClientRoutes);
