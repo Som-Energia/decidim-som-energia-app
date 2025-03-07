7fbce01d4bc1107fb1b57e71e48ed56473139285
Ivan Vergés <ivan@pokecode.net>
2025-03-07 11:34:43 +0100

    healthcheck config

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-if ENV.fetch("HEALTHCHECK_ADDITIONAL_CHECKS", nil)
+if (additional = ENV.fetch("HEALTHCHECK_ADDITIONAL_CHECKS", nil))
   HealthCheck.setup do |config|
-    config.standard_checks += ENV.fetch("HEALTHCHECK_ADDITIONAL_CHECKS", nil).split
+    config.standard_checks += additional.split
   end
 end
 
-if ENV.fetch("HEALTHCHECK_EXCLUDE_CHECKS", nil)
+if (exclude = ENV.fetch("HEALTHCHECK_EXCLUDE_CHECKS", "emailconf"))
   HealthCheck.setup do |config|
-    config.standard_checks -= ENV.fetch("HEALTHCHECK_EXCLUDE_CHECKS", "emailconf").split
+    config.standard_checks -= exclude.split
   end
 end
