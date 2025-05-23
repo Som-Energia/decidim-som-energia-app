9873c2e7619f7be704829d7e2353ec385e2903b6
Ivan Vergés <ivan@pokecode.net>
2025-05-23 14:25:28 +0200

    tune modals and redirects to cas

--- app/packs/src/decidim/decidim_application.js
+++ app/packs/src/decidim/decidim_application.js
@@ -6,3 +6,21 @@ import "src/consultations_question_requirements_validator";
 // Load images
 require.context("../../images", true);
 
+const bindCasClientRoutes = () => {
+  let locale = document.documentElement.getAttribute("lang") || "es";
+
+  document.querySelectorAll('a[href*="/users/sign_in"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_in?locale=${locale}`);
+    element.setAttribute("data-method", "post");
+  });
+  document.querySelectorAll('a[href*="/users/sign_up"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_in?locale=${locale}`);
+  });
+  document.querySelectorAll('a[href*="/users/sign_out"]').forEach((element) => {
+    element.setAttribute("href", `/users/cas/sign_out?locale=${locale}`);
+  });
+  Rails.start();
+}
+
+
+document.addEventListener("DOMContentLoaded", bindCasClientRoutes);
